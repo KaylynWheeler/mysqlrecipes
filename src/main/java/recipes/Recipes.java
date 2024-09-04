d4f675899bbc40fdb486ab1d233a3cc25986ec33
Kaylyn Wheeler <you@example.com>
2024-09-03 20:19:16 -0500

    first commit

---
+++ src/main/java/recipes/Recipes.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package recipes;
+
+import java.sql.Connection;
+import recipes.dao.DbConnection;
+
+/**
+ * 
+ */
+public class Recipes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+    DbConnection.getConnection();
+
+	}
+
+}
